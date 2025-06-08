aee126bf6f30f457a102152863f88e48c10cbd99
ThomasDHolt <tholt9111@gmail.com>
2025-06-08 21:16:25 +0100

    Remake the codebase to be more manageable.

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const imageSelector = document.getElementById("imageSelector");
-const imageCanvas = document.getElementById("imageCanvas");
+const imageSelector = document.getElementById("thumbnailContainer");
+const imageCanvas = document.getElementById("imageContainer");
 const previousButton = document.getElementById("previousButton");
 const nextButton = document.getElementById("nextButton");
 
 const images = [
     hubbleDeepField = {
         imageSrc: "./images/HubbleDeepField/HubbleDeepFieldNative.webp",
+        alt: "The Hubble Deep Field which was captured in 1995",
         LODs: [
             "./images/HubbleDeepField/LODs/HubbleDeepField-1000px.webp",
             "./images/HubbleDeepField/LODs/HubbleDeepField-500px.webp"
@@ -13,6 +14,7 @@ const images = [
     },
     EarthRise = {
         imageSrc: "./images/EarthRise/EarthRise.webp",
+        alt: "The Earthrise photo which was captured during the Apollo 8 mission in 1968",
         LODs: [
             "./images/EarthRise/LODs/EarthRise-1000px.webp",
             "./images/EarthRise/LODs/EarthRise-500px.webp"
@@ -20,6 +22,7 @@ const images = [
     },
     SombreroGalaxy = {
         imageSrc: "./images/SombreroGalaxy/SombreroGalaxy.webp",
+        alt: "The Sombrero Galaxy which is 29.35 million Light Years away from Earth",
         LODs: [
             "./images/SombreroGalaxy/LODs/SombreroGalaxy-1000px.webp",
             "./images/SombreroGalaxy/LODs/SombreroGalaxy-500px.webp"
@@ -29,15 +32,50 @@ const images = [
 
 let imageIterator = 0;
 
-let selectedImage = document.createElement("img");
-selectedImage.src = images[imageIterator].imageSrc;
-imageCanvas.appendChild(selectedImage);
-
-
-for(let i = 0; i < images.length; i++)
+function CreateThumbnails()
 {
-    let newThumbnail = document.createElement("img");
-    newThumbnail.src = images[i].imageSrc;
-    newThumbnail.classList.add("thumbnail");
-    imageSelector.appendChild(newThumbnail);
+    images.forEach(function (image, index) {
+        let newThumbnail = document.createElement("img");
+        newThumbnail.src = image.imageSrc;
+        newThumbnail.alt = image.alt;
+        newThumbnail.classList.add("thumbnail");
+
+        newThumbnail.addEventListener("click", function () {
+            CreateSelectedImage(image);
+            imageIterator = index;
+        })
+
+        imageSelector.appendChild(newThumbnail);
+    })
 }
+
+CreateThumbnails();
+
+function CreateSelectedImage(imageDetails)
+{
+    imageCanvas.innerHTML = "";
+    let selectedImage = document.createElement("img");
+
+    console.log(selectedImage);
+
+    selectedImage.src = imageDetails.imageSrc;
+    selectedImage.alt = imageDetails.alt;
+
+    imageCanvas.appendChild(selectedImage);
+}
+
+previousButton.addEventListener("click", function () {
+    imageIterator--;
+
+    if(imageIterator < 0) {imageIterator = images.length - 1;}
+
+    CreateSelectedImage(images[imageIterator]);
+});
+
+nextButton.addEventListener("click", function () {
+    imageIterator++;
+
+    if(imageIterator == images.length) {imageIterator = 0;}
+
+    CreateSelectedImage(images[imageIterator]);
+});
